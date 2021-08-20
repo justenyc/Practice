f05e4b4bd1f24fcb6224a30bad0f29bdb3ea6189
justenyc <justen.chong@gmail.com>
2021-08-20 11:07:50 -0500

    Audio sync script 1.0

---
+++ Assets/trunk/Scripts/Universal/FirstPersonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 1;
+    [SerializeField] float _rotateSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            Camera.main.transform.position += Camera.main.transform.right * Input.GetAxis("Horizontal")* Time.deltaTime * _moveSpeed;
+        }
+
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
+        {
+            Camera.main.transform.position += Camera.main.transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * _moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Camera.main.transform.position += Camera.main.transform.up * Time.deltaTime * _moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Camera.main.transform.position += -Camera.main.transform.up * Time.deltaTime * _moveSpeed;
+        }
+
+    }
+}
